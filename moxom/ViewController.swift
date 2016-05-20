6081323b39c66e49e5a831e7a6728b3c3fa9318c
Santosh <santosh@Mcoms-MacBook-Pro.local>
2016-05-20 15:56:04 +0800

    Table View

--- moxom/ViewController.swift
+++ moxom/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController:UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var homeTable: UITableView!
+    
+    var items: [String] = ["one","two","three"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.homeTable.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+//    override func tableView(tableView: UITableView!, heightForRowAtIndexPath indexPath: NSIndexPath!) -> CGFloat {
+//        var cell = tableView.cellForRowAtIndexPath(indexPath)
+//        return cell.frame.height
+//    }
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+    {
+        return 500.0;//Choose your custom row height
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell =
+            self.homeTable.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("You selected cell #\(indexPath.row)!")
+    }
 }
-
